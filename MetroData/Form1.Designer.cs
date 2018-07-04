fb523ea8fe2779d5c08fe9d266819dc4e9091172
unknown <rzhan016@ucr.edu>
2018-07-03 17:21:00 -0700

    filled seq, added xml url

--- MetroData/Form1.Designer.cs
+++ MetroData/Form1.Designer.cs
@@ -28,21 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ConnectionLabel = new System.Windows.Forms.Label();
+            this.layoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.databaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableDisplay = new System.Windows.Forms.DataGridView();
+            this.layoutPanel.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tableDisplay)).BeginInit();
             this.SuspendLayout();
+            // 
+            // ConnectionLabel
+            // 
+            this.ConnectionLabel.AutoSize = true;
+            this.ConnectionLabel.Location = new System.Drawing.Point(533, 144);
+            this.ConnectionLabel.Name = "ConnectionLabel";
+            this.ConnectionLabel.Size = new System.Drawing.Size(0, 13);
+            this.ConnectionLabel.TabIndex = 0;
+            // 
+            // layoutPanel
+            // 
+            this.layoutPanel.ColumnCount = 1;
+            this.layoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.layoutPanel.Controls.Add(this.menuStrip1, 0, 0);
+            this.layoutPanel.Controls.Add(this.textBox1, 0, 1);
+            this.layoutPanel.Controls.Add(this.tableDisplay, 0, 2);
+            this.layoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.layoutPanel.Name = "layoutPanel";
+            this.layoutPanel.RowCount = 4;
+            this.layoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.layoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.layoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.layoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 112F));
+            this.layoutPanel.Size = new System.Drawing.Size(674, 396);
+            this.layoutPanel.TabIndex = 1;
+            this.layoutPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.tableDisplay_Paint);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // databaseToolStripMenuItem
+            // 
+            this.databaseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToolStripMenuItem,
+            this.disconnectToolStripMenuItem});
+            this.databaseToolStripMenuItem.Name = "databaseToolStripMenuItem";
+            this.databaseToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.databaseToolStripMenuItem.Text = "Database";
+            // 
+            // connectToolStripMenuItem
+            // 
+            this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
+            this.connectToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.connectToolStripMenuItem.Text = "Connect";
+            this.connectToolStripMenuItem.Click += new System.EventHandler(this.connectToolStripMenuItem_Click);
+            // 
+            // disconnectToolStripMenuItem
+            // 
+            this.disconnectToolStripMenuItem.Name = "disconnectToolStripMenuItem";
+            this.disconnectToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.disconnectToolStripMenuItem.Text = "Disconnect";
+            this.disconnectToolStripMenuItem.Click += new System.EventHandler(this.disconnectToolStripMenuItem_Click);
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.runToolStripMenuItem.Text = "Run";
+            this.runToolStripMenuItem.Click += new System.EventHandler(this.runToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.databaseToolStripMenuItem,
+            this.runToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(674, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(668, 123);
+            this.textBox1.TabIndex = 1;
+            // 
+            // tableDisplay
+            // 
+            this.tableDisplay.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableDisplay.Location = new System.Drawing.Point(3, 157);
+            this.tableDisplay.Name = "tableDisplay";
+            this.tableDisplay.ReadOnly = true;
+            this.tableDisplay.RowHeadersVisible = false;
+            this.tableDisplay.Size = new System.Drawing.Size(668, 123);
+            this.tableDisplay.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(674, 396);
+            this.Controls.Add(this.layoutPanel);
+            this.Controls.Add(this.ConnectionLabel);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.layoutPanel.ResumeLayout(false);
+            this.layoutPanel.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tableDisplay)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label ConnectionLabel;
+        private System.Windows.Forms.TableLayoutPanel layoutPanel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem databaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disconnectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView tableDisplay;
     }
 }
 
--- MetroData/Form1.cs
+++ MetroData/Form1.cs
@@ -13,12 +13,101 @@ namespace MetroData
 {
     public partial class Form1 : Form
     {
+        private string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\Documents\RuifengSummerJob\MetroData\MetroDatabase.accdb;Persist Security Info=False;";
+        private OleDbConnection connection;
+
         public Form1()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+            connectToolStripMenuItem.PerformClick();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void connectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection = new OleDbConnection(connectionString);
+                connection.Open();
+                disconnectToolStripMenuItem.Enabled = true;
+                connectToolStripMenuItem.Enabled = false;
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void disconnectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection = new OleDbConnection(connectionString);
+                connection.Close();
+                disconnectToolStripMenuItem.Enabled = false;
+                connectToolStripMenuItem.Enabled = true;
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void runToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                OleDbCommand cmd = new OleDbCommand();
+                cmd.Connection = connection;
+                OleDbDataAdapter dataAdapter = new OleDbDataAdapter("SELECT ID, SERIAL_NBR, [Transaction DTM], LATITUDE, LONGITUDE, RouteID, DateStamp, SequenceID, Time, URL FROM [2018-3-24]", connection);
+                DataTable data = new DataTable();
+                dataAdapter.Fill(data);
+                // For each row, check SERIAL_NBR
+                object sNNR = 0;
+                int count = 0;
+                DataRow prevRow = data.Rows[0];
+                DataRow firstRow = data.Rows[0];
+                //Console.WriteLine(prevNBR);
+                foreach (DataRow row in data.Rows)
+                {
+                    if (row[1].Equals(sNNR))
+                    {
+                        count++;
+                        cmd.CommandText = "UPDATE[2018-3-24] SET[2018-3-24].SequenceID = " + count + " WHERE [2018-3-24].[ID] = " + row[0] + ";";
+                        cmd.ExecuteNonQuery();
+                        cmd.CommandText = "UPDATE[2018-3-24]" + " SET[2018-3-24].URL = " + "\"http://api.metrocloudalliance.com/route/?from_place=" + prevRow[3] + "," + prevRow[4] + "&to_place=" + row[3] + "," + row[4] + "&date=" + prevRow[6] + "&time=" + prevRow[8] + "&mode=TRANSIT,WALK&max_itineraries=4&output_format=xml&api_key=Lvj4Y3icznHjPSWT8HjU\"" + " WHERE [2018-3-24].[ID] = " + prevRow[0] + ";";
+                        //Console.WriteLine(cmd.CommandText);
+                        cmd.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        count = 1;
+                        sNNR = row[1];
+                        cmd.CommandText = "UPDATE[2018-3-24] SET[2018-3-24].SequenceID = " + count + " WHERE [2018-3-24].[ID] = " + row[0] + ";";
+                        cmd.ExecuteNonQuery();
+                        cmd.CommandText = "UPDATE[2018-3-24]" + " SET[2018-3-24].URL = " + "\"http://api.metrocloudalliance.com/route/?from_place=" + prevRow[3] + "," + prevRow[4] + "&to_place=" + firstRow[3] + "," + firstRow[4] + "&date=" + prevRow[6] + "&time=" + prevRow[8] + "&mode=TRANSIT,WALK&max_itineraries=4&output_format=xml&api_key=Lvj4Y3icznHjPSWT8HjU\"" + " WHERE [2018-3-24].[ID] = " + prevRow[0] + ";";
+                        //Console.WriteLine(cmd.CommandText);
+                        cmd.ExecuteNonQuery();
+                        firstRow = row;
+                    }
+                    prevRow = row;
+                }
+                prevRow = data.Rows[data.Rows.Count - 1];
+                cmd.CommandText = "UPDATE[2018-3-24]" + " SET[2018-3-24].URL = " + "\"http://api.metrocloudalliance.com/route/?from_place=" + prevRow[3] + "," + prevRow[4] + "&to_place=" + firstRow[3] + "," + firstRow[4] + "&date=" + prevRow[6] + "&time=" + prevRow[8] + "&mode=TRANSIT,WALK&max_itineraries=4&output_format=xml&api_key=Lvj4Y3icznHjPSWT8HjU\"" + " WHERE [2018-3-24].[ID] = " + prevRow[0] + ";";
+                cmd.ExecuteNonQuery();
+                //dataAdapter.Update(data);
+                tableDisplay.DataSource = data;
+                tableDisplay.AutoResizeColumns();
+
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+            
+        }
+
+        private void tableDisplay_Paint(object sender, PaintEventArgs e)
         {
 
         }
