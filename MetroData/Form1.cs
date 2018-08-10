95f7a60eaa6c71409602b536cef8a33c7d38c140
unknown <rzhan016@ucr.edu>
2018-08-09 19:31:12 -0700

    Add bus routes and missing routes

--- MetroData/Form1.cs
+++ MetroData/Form1.cs
@@ -73,18 +73,8 @@ namespace MetroData
                 int count = 0;
                 DataRow prevRow = data.Rows[0];
                 DataRow firstRow = data.Rows[0];
-                XDocument xml = new XDocument();
-                //string xmltest = "http://api.metrocloudalliance.com/route/?from_place=34.14942,-118.64722&to_place=34.18606,-118.50103&date=3/24/2017&time=15:56:21&mode=TRANSIT,WALK&max_itineraries=4&output_format=xml&api_key=Lvj4Y3icznHjPSWT8HjU";
-                //xml = XDocument.Load(xmltest);
-                //Console.WriteLine(xml.ToString());
-                
-                //cmd.CommandText = "UPDATE[2018-3-24]" + " SET[2018-3-24].URL = " + "\"" + 23 + "\"" + ", " + "[2018-3-24].XML = " + "\"" + XElement.Load(xmltest) + "\"" + " WHERE [2018-3-24].[ID] = " + "2" + ";";
-                //Console.WriteLine();
-
-                //cmd.CommandText = "UPDATE[2018-3-24] SET[2018-3-24].URL = " + "\"" + 8 + "\"" + ", " + "[2018-3-24].XML = " + "\"" + @parseXml(xml.ToString()) + "\"" + " WHERE [2018-3-24].[ID] = " + 1 + ";";
-                //Console.WriteLine(cmd.CommandText);
-                //cmd.ExecuteNonQuery();
-
+                string xmlll = @"http://api.metrocloudalliance.com/route/?from_place=34.07275,-118.045138888889&to_place=34.1405555555556,-118.141388888889&date=3/10/2017&time=06:54:02&mode=TRANSIT,WALK&max_itineraries=4&output_format=xml&api_key=Lvj4Y3icznHjPSWT8HjU";
+                XDocument.Load(xmlll);
                 foreach (DataRow row in data.Rows)
                 {
                     row[2] = dmsToDegLat(row[2].ToString());
@@ -92,29 +82,31 @@ namespace MetroData
                     if (row[1].Equals(sNNR))
                     {
                         count++;
-                        row[8] = "http://api.metrocloudalliance.com/route/?from_place=" + prevRow[2] + "," + prevRow[3] + "&to_place=" + row[2] + "," + row[3] + "&date=" + prevRow[5].ToString().Split(' ')[0] + "&time=" + prevRow[7] + "&mode=TRANSIT,WALK&max_itineraries=4&output_format=xml&api_key=Lvj4Y3icznHjPSWT8HjU";
-                        cmd.CommandText = "UPDATE[2018-3-24] SET[2018-3-24].URL = " + "\"" + row[8] + "\"" + ", " + "[2018-3-24].XML = " + "\"" + @parseXml(XDocument.Load(row[8].ToString()).ToString()) + "\"" + " WHERE [2018-3-24].[ID] = " + prevRow[0] + ";";
-                        cmd.ExecuteNonQuery();
+                        row[8] = parseURL(prevRow[2], prevRow[3], row[2], row[3], prevRow[5], prevRow[7]);
+                        //cmd.CommandText = "UPDATE[2018-3-24] SET[2018-3-24].URL = " + "\"" + row[8] + "\"" + ", " + "[2018-3-24].XML = " + "\"" + @parseXml(XDocument.Load(row[8].ToString())) + "\"" + " WHERE [2018-3-24].[ID] = " + prevRow[0] + ";";
                     }
                     else
                     {
                         count = 1;
                         sNNR = row[1];
-                        row[8] = "http://api.metrocloudalliance.com/route/?from_place=" + prevRow[2] + "," + prevRow[3] + "&to_place=" + firstRow[2] + "," + firstRow[3] + "&date=" + prevRow[5].ToString().Split(' ')[0] + "&time=" + prevRow[7] + "&mode=TRANSIT,WALK&max_itineraries=4&output_format=xml&api_key=Lvj4Y3icznHjPSWT8HjU";
-                        cmd.CommandText = "UPDATE[2018-3-24] SET[2018-3-24].URL = " + "\"" + row[8] + "\"" + ", " + "[2018-3-24].XML = " + "\"" + @parseXml(XDocument.Load(row[8].ToString()).ToString()) + "\"" + " WHERE [2018-3-24].[ID] = " + prevRow[0] + ";";
-                        cmd.ExecuteNonQuery();
+                        row[8] = parseURL(prevRow[2], prevRow[3], firstRow[2], firstRow[3], prevRow[5], prevRow[7]);
+                        //cmd.CommandText = "UPDATE[2018-3-24] SET[2018-3-24].URL = " + "\"" + row[8] + "\"" + ", " + "[2018-3-24].XML = " + "\"" + @parseXml(XDocument.Load(row[8].ToString())) + "\"" + " WHERE [2018-3-24].[ID] = " + prevRow[0] + ";";
                         firstRow = row;
                     }
+                    Console.WriteLine(row[8].ToString());
+                    Console.WriteLine(row[4].ToString());
+                    findRoute(XDocument.Load(row[8].ToString()), row[4].ToString());
+                    cmd.CommandText = "UPDATE[2018-3-24] SET[2018-3-24].URL = " + "\"" + row[8] + "\"" + ", " + "[2018-3-24].XML = " + "\"HEY\"" + " WHERE [2018-3-24].[ID] = " + prevRow[0] + ";";
+                    //cmd.ExecuteNonQuery();
                     cmd.CommandText = "UPDATE[2018-3-24] SET[2018-3-24].SequenceID = " + count + ", " + "[2018-3-24].LATITUDE_C = " + row[2] + ", " + "[2018-3-24].LONGITUDE_C = " + row[3] + " WHERE [2018-3-24].[ID] = " + row[0] + ";";
-                    cmd.ExecuteNonQuery();
+                    //cmd.ExecuteNonQuery();
                     prevRow = row;
                 }
-
                 prevRow = data.Rows[data.Rows.Count - 1];
-                prevRow[8] = "http://api.metrocloudalliance.com/route/?from_place=" + prevRow[2] + "," + prevRow[3] + "&to_place=" + firstRow[2] + "," + firstRow[3] + "&date=" + prevRow[5].ToString().Split(' ')[0] + "&time=" + prevRow[7] + "&mode=TRANSIT,WALK&max_itineraries=4&output_format=xml&api_key=Lvj4Y3icznHjPSWT8HjU";
-                cmd.CommandText = "UPDATE[2018-3-24] SET[2018-3-24].URL = " + "\"" + prevRow[8] + "\"" + ", " + "[2018-3-24].XML = " + "\"" + @parseXml(XDocument.Load(prevRow[8].ToString()).ToString()) + "\"" + " WHERE [2018-3-24].[ID] = " + prevRow[0] + ";";
-                cmd.ExecuteNonQuery();
-                dataAdapter.Update(data);
+                prevRow[8] = parseURL(prevRow[2], prevRow[3], firstRow[2], firstRow[3], prevRow[5], prevRow[7]);
+                //cmd.CommandText = "UPDATE[2018-3-24] SET[2018-3-24].URL = " + "\"" + prevRow[8] + "\"" + ", " + "[2018-3-24].XML = " + "\"" + @parseXml(XDocument.Load(prevRow[8].ToString())) + "\"" + " WHERE [2018-3-24].[ID] = " + prevRow[0] + ";";
+                //cmd.ExecuteNonQuery();
+                //dataAdapter.Update(data);
                 tableDisplay.DataSource = data;
                 tableDisplay.AutoResizeColumns();
             }
@@ -137,23 +129,69 @@ namespace MetroData
             return Math.Round(Convert.ToDouble(dms[0]) - Convert.ToDouble(dms[1]) / 60.0 - Convert.ToDouble(dms[2]) / 3600.0, 5);
         }
 
-        private string parseXml(string xml)
+
+        private string findRoute(XDocument xml, string route)
         {
-            StringReader reader = new StringReader(xml);
-            string xmlLine, xmlFull = "";
-            while (true)
+            if (xml.Descendants("itineraries").Count() < 1)
+                return "FAIL";
+            var itineraries = xml.Descendants("itineraries").First();
+            //Console.WriteLine(itineraries);
+            var routenum = route.Substring(6).PadLeft(3, '0');
+
+            Console.WriteLine("---------------------------------------------------------------------");
+
+            foreach (var item in itineraries.Elements())
             {
-                xmlLine = reader.ReadLine();
-                if (xmlLine != null)
+                Console.WriteLine(item.Descendants("route_short_name").Count());
+                //Console.WriteLine(item);
+                //Console.WriteLine(items.Descendants("route"));
+                foreach (var routes in item.Descendants("route_short_name"))
                 {
-                    xmlFull = xmlFull + xmlLine + "\r\n";
+                    Console.WriteLine("R1: " + routes.Value);
+                    Console.WriteLine("R2: " + routenum);
+                    if (routes.Value.Equals(routenum))
+                    {
+                        if(item.Descendants("route_short_name").Count() == 1)
+                        {
+                            return "route";
+                        }
+                        else
+                        {
+                            Console.WriteLine(item.Descendants("route_short_name"));
+                        }
+
+                    }
+                    //Console.WriteLine(routes.Value);
                 }
-                else
-                {
-                    break;
-                }
+                //Console.WriteLine("NEXT");
             }
-            return xmlFull;
+            //itineraries.Element("item0");
+            return "";
+        }
+
+        private string parseXml(XDocument xml)
+        {
+            //StringReader reader = new StringReader(xml);
+            //string xmlLine, xmlFull = "";
+            //while (true)
+            //{
+            //    xmlLine = reader.ReadLine();
+            //    if (xmlLine != null)
+            //    {
+            //        xmlFull = xmlFull + xmlLine + "\r\n";
+            //    }
+            //    else
+            //    {
+            //        break;
+            //    }
+            //}
+            //return xmlFull;
+            return "";
+        }
+
+        private string parseURL(object lat1, object lon1, object lat2, object lon2, object date, object time)
+        {
+            return "http://api.metrocloudalliance.com/route/?from_place=" + lat1 + "," + lon1 + "&to_place=" + lat2 + "," + lon2 + "&date=" + date.ToString().Split(' ')[0] + "&time=" + time + "&mode=TRANSIT,WALK&max_itineraries=4&output_format=xml&api_key=Lvj4Y3icznHjPSWT8HjU";
         }
 
         private void tableDisplay_Paint(object sender, PaintEventArgs e)
